efad936a8014552d8aaa71df138ddbe4588950dd
Ivana Petrovic <Ivana.Petrovic1@rs.ey.com>
2022-03-05 18:29:04 +0100

    responsive header completed

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  //   function changeDropdownIcon(x) {
+  //     x.classList.remove("fa-plus");
+  //     x.classList.add("fa-minus");
+  //   }
+
+  $(".dropdown-menu").onmouseleave(function () {
+    this.css("background-color", "$misty-blue");
+  });
+});
